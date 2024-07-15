d89f642c19f80bc2fb966bbe96e7ae3c4a6bb86d
Robosapien737 <91835339+Robosapien737@users.noreply.github.com>
2024-07-15 14:49:47 -0700

    Update WallpaperRunner.bat

--- WallpaperRunner/WallpaperRunner.bat
+++ WallpaperRunner/WallpaperRunner.bat
@@ -1,3 +1,3 @@
-powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Robosapien737/Ducky-Stuff/main/Wallpaper%20Prank/minion.jpg" -OutFile %temp%\minion.jpg
+powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Robosapien737/Ducky-Stuff/main/WallpaperRunner/minion.jpg" -OutFile %temp%\minion.jpg
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%temp%\minion.jpg" /f
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters
